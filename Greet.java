e98de302f7d377d1d4a67d25b8f3192586ff95c7
Mbulelo DAMBA <mdamba@e4r13p7.wethinkcode.co.za>
2018-01-28 16:42:53 +0200

    Java Projects

---
+++ Greet.java
@@ -0,0 +1,6 @@
+
+public class Greet{
+	public void hello(String name) {
+		System.out.println("Hello "+name+", how you're doing today?");
+	}	
+}
---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main (String[] args) {
+		Greet greetings = new Greet();
+	//	Systems.out.println("Hello World!!");
+	//	Systems.out.println("Hello World!!");
+		greetings.hello(args[0]);
+		System.out.println();
+	}
+}
